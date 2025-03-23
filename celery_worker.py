5b0504d5cb366187581aa58dd5ac526853379fa0
cdownie75 <chris@pasabi.com>
2025-03-23 01:31:18 +0000

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -9,8 +9,6 @@ celery = Celery(
     "tasks",
     broker=os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0"),
     backend=os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
-),
-    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 )
 
 def preprocess_image_for_ocr(image_path):
